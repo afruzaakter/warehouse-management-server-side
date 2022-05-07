04376548adf5640730e98f3ea9cc2d055bb69ba2
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-05-07 20:00:38 +0600

    create post data base server and client side

--- index.js
+++ index.js
@@ -20,39 +20,55 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-           await client.connect();
-           const serviceCollection = client.db('dressUp').collection('inventoryItems');
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('dressUp').collection('inventoryItems');
 
-          app.get('/service', async(req, res) =>{
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-          });
+        });
 
-          app.get('/service/:id', async(req, res)=>{
-              const id = req.params.id;
-              const query = {_id: ObjectId(id)};
-              const service = await serviceCollection.findOne(query);
-              res.send(service);
-          })
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
 
-          app.post('/products', async(req, res) =>{
-               const product = req.body;
-               console.log('add new user',product);
-                // res.send({result: ' user data receved'});
-              
+        app.post('/service', async (req, res) => {
+            const product = req.body;
+            console.log('add new user', product);
+            // res.send({result: ' user data receved'});
 
-               if(!product.name || !product.price ){
-                   return res.send({success: false, error: "Please Provide all information"})
-               }
-               const result = await serviceCollection.insertOne(product);
-          });
+
+            if (!product.name || !product.price || !product.quantity || !product.suppler || !product.image ||!product.description) {
+                return res.send({ success: false, error: "Please Provide all information" })
+            }
+            const result = await serviceCollection.insertOne(product);
+            res.send({ success: true, message: `Successfully inserted ${product.name}!` });
+        });
+
+        app.get('/service', async (req, res) => {
+            
+            const limit = Number(req.query.limit);
+            // console.log(limit);
+            const cursor = serviceCollection.find();
+            const products = await cursor.limit(limit).toArray();
+           
+            if (!products?.length) {
+                return res.send({ success: false, error: "No product found" })
+            }
+            res.send({ success: true, data: products});
+            
+        })
+
     }
-   finally{
-       
+    finally {
+
 
     }
 
@@ -61,9 +77,9 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Running Server')
 });
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
